68a6207206959cbfcbfc621a853e0f0c78f20386
Guodong Wu <GuodonWu@163.com>
2019-02-25 15:04:22 +0800

    add branch guodong wu

--- src/test/java/edu/seu/housepricepredict/HousePricePredictApplicationTests.java
+++ src/test/java/edu/seu/housepricepredict/HousePricePredictApplicationTests.java
@@ -15,6 +15,7 @@ public class HousePricePredictApplicationTests {
         System.out.println("branch01");
         System.out.println("branch02");
         System.out.println("branch01: day2");
+        System.out.println("branch guodong wu");
     }
 
 }
