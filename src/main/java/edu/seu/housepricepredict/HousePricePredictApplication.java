30912faac6ca416b06fc263beab0eee790b8267a
Guodong Wu <GuodonWu@163.com>
2019-02-25 14:40:21 +0800

    Initial commit

---
+++ src/main/java/edu/seu/housepricepredict/HousePricePredictApplication.java
@@ -0,0 +1,13 @@
+package edu.seu.housepricepredict;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HousePricePredictApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HousePricePredictApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/seu/housepricepredict/HousePricePredictApplicationTests.java
@@ -0,0 +1,17 @@
+package edu.seu.housepricepredict;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HousePricePredictApplicationTests {
+
+    @Test
+    public void contextLoads() {
+        System.out.println("success");
+    }
+
+}
