80c5ec3022d3c957894f89bbc89854d4708b81ac
Guodong Wu <GuodonWu@163.com>
2019-03-03 11:12:58 +0800

    finish Area Query

--- src/main/java/edu/seu/housepricepredict/mapper/StreetMapper.java
+++ src/main/java/edu/seu/housepricepredict/mapper/StreetMapper.java
@@ -1,7 +1,12 @@
 package edu.seu.housepricepredict.mapper;
 
 import edu.seu.housepricepredict.domain.pojo.area.Street;
+import edu.seu.housepricepredict.domain.vo.area.StreetAreaVo;
+import org.apache.ibatis.annotations.Many;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.mapping.FetchType;
 
 import java.util.List;
 
@@ -18,4 +23,20 @@ public interface StreetMapper {
      */
     @Select("SELECT s_id, s_name, s_price FROM street WHERE d_id = #{id}")
     List<Street> getStreetsBydId(int id);
+
+    /**
+     * 根据街道id，查找街道信息以及其下的小区
+     */
+    @Select("Select s_id, s_name, s_price FROM street WHERE s_id = #{id}")
+    @Results({
+            @Result(id = true, column ="s_id", property = "sId"),
+            @Result(column = "s_id", property = "communities", javaType = List.class,
+                    many = @Many(
+                            select = "edu.seu.housepricepredict.mapper.CommunityMapper.getCommunityBysId",
+                            fetchType = FetchType.LAZY
+                    )
+            )}
+
+    )
+    StreetAreaVo getStreetAreaById(int id);
 }
