a83e0fee655238b2fa6e721c2947b97192cea76a
tom_pro <tom_pro>
2019-03-05 17:50:14 +0800

    增加了柱状图折线图以及城市列表

--- src/main/java/edu/seu/housepricepredict/domain/pojo/ValueOfxy.java
+++ src/main/java/edu/seu/housepricepredict/domain/pojo/ValueOfxy.java
@@ -1,4 +1,40 @@
 package edu.seu.housepricepredict.domain.pojo;
 
 public class ValueOfxy {
+    private String x;
+    private Integer y1;
+    private Integer y2;
+
+    public ValueOfxy() {
+    }
+
+    public ValueOfxy(String x, Integer y1, Integer y2) {
+        this.x = x;
+        this.y1 = y1;
+        this.y2 = y2;
+    }
+
+    public String getX() {
+        return x;
+    }
+
+    public void setX(String x) {
+        this.x = x;
+    }
+
+    public Integer getY1() {
+        return y1;
+    }
+
+    public void setY1(Integer y1) {
+        this.y1 = y1;
+    }
+
+    public Integer getY2() {
+        return y2;
+    }
+
+    public void setY2(Integer y2) {
+        this.y2 = y2;
+    }
 }
