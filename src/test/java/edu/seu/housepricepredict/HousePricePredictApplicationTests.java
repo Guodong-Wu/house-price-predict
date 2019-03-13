56fe3e3c60d13cac628f72a4b11b17eb6e1df05f
Guodong Wu <GuodonWu@163.com>
2019-03-13 17:47:59 +0800

    delete index

---
+++ src/test/java/edu/seu/housepricepredict/HousePricePredictApplicationTests.java
@@ -0,0 +1,84 @@
+package edu.seu.housepricepredict;
+
+import edu.seu.housepricepredict.service.CityService;
+import edu.seu.housepricepredict.service.ImportDataService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.IOException;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HousePricePredictApplicationTests {
+
+    @Autowired
+    ImportDataService importDataService;
+
+    @Autowired
+    CityService cityService;
+
+    @Test
+    public void testCityMapper() {
+
+    }
+
+    @Test
+    public void testDistrictMapper() {
+
+    }
+
+    @Test
+    public void testStreetMapper() {
+
+    }
+
+    @Test
+    public void insertCity() throws IOException {
+
+    }
+
+    @Test
+    public void insertDistrict() throws IOException {
+
+    }
+
+    @Test
+    public void insertStreet() throws IOException {
+
+    }
+
+    @Test
+    public void insertStreetMonthPrice() throws IOException {
+        importDataService.insertStreetMonthPrice();
+    }
+
+    @Test
+    public void insertDistrictMonthPrice() {
+
+    }
+
+    @Test
+    public void insertCityMonthPrice() {
+
+    }
+
+    @Test
+    public void updateCityPrice() {
+
+    }
+
+    @Test
+    public void updateDistrictPrice() {
+
+    }
+
+    @Test
+    public void updateStreetPrice() {
+
+    }
+
+
+}
