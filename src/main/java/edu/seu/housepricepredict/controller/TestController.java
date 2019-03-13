1cdfe88121a7e5085990c70523261fc1d2e31ae0
tom_pro <tom_pro>
2019-03-13 10:09:22 +0800

    edit structure of web page

---
+++ src/main/java/edu/seu/housepricepredict/controller/TestController.java
@@ -0,0 +1,105 @@
+package edu.seu.housepricepredict.controller;
+
+import edu.seu.housepricepredict.utils.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author xg_song
+ * @data 2019/3/11 10:32
+ */
+@Controller
+public class TestController {
+
+    @RequestMapping("/map")
+    public String toMapPage(){
+        return "map";
+    }
+
+    @RequestMapping("/test-map")
+    public String toTestPage(){
+        return "test-map";
+    }
+
+    /**
+     * 获取表单选择的两个城市名，并跳转到折线图页面。
+     * @param commName1
+     * @param commName2
+     * @return
+     */
+    @RequestMapping(value = "/lineChart")
+    public String toComparePage(String commName1,  String commName2, Model model){
+        model.addAttribute("commName1", commName1);
+        model.addAttribute("commName2", commName2);
+        return "lineChart";
+    }
+
+    @RequestMapping("/choose")
+    public String chooseCity(){
+        return "choose";
+    }
+
+    /**
+     * 返回该区域内小区列表
+     * @return
+     */
+    @RequestMapping("/getCityNames")
+    @ResponseBody
+    public String[] getCityNames(){
+        String [] cityNames = {"1", "2", "3", "4"};
+        return cityNames;
+    }
+
+    /**
+     * 取出两个小区名，并将小区历史房价放入Value数组传到页面
+     * Value属性：
+     *          dataOfx:横坐标数据---时间
+     *          dataOfy1:纵坐标数据---小区1房价
+     *          dataOfy2：纵坐标数据---小区2房价
+     * @param request
+     * @return
+     */
+    @RequestMapping("/getPriceOfComm")
+    @ResponseBody
+    public Value[] getPriceOfComm(HttpServletRequest request){
+        //小区名一：commName1, 小区名二：commName2
+        String commName1 = request.getParameter("comm1");
+        String commName2 = request.getParameter("comm2");
+        Value [] values = new Value[8];
+        for(int i = 0; i < 8; i++){
+            values[i] = new Value(i + "", i + "", i + 2 + "");
+        }
+        return values;
+    }
+
+    /**
+     * 获取要对比小区所在城市并返回
+     * @return
+     */
+    @RequestMapping("/getCityName")
+    @ResponseBody
+    public String[] getCityName(){
+        String cityName[] = {"苏州市"};
+        System.out.println("getCityName " + cityName[0]);
+
+        return cityName;
+    }
+
+    /**
+     * 获取要对比小区所在区域和街道并返回。
+     * @return
+     */
+    @RequestMapping("/getAddress")
+    @ResponseBody
+    public String[] getAddress(){
+        String address[] = new String[1];
+//        address[0] = "吴中区文荟人才公寓";
+        address[0] = "吴中区文星广场";
+        System.out.println("getAddress " + address[0]);
+        return address;
+    }
+
+}
