c45560b17f4d580976d9e019befbe5de37ae51d5
tom_pro <tom_pro>
2019-03-14 16:51:55 +0800

    improve pages

--- src/main/java/edu/seu/housepricepredict/controller/TestController.java
+++ src/main/java/edu/seu/housepricepredict/controller/TestController.java
@@ -1,6 +1,7 @@
 package edu.seu.housepricepredict.controller;
 
 import edu.seu.housepricepredict.domain.Value;
+import edu.seu.housepricepredict.domain.user.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -93,4 +94,73 @@ public class TestController {
         return address;
     }
 
+    /**
+     * 查询用户是否登录，若登录则返回用户信息，否则返回空
+     */
+    @RequestMapping("/getUserInfo")
+    @ResponseBody
+    public String getUserInfo(){
+        User user = null;
+        user = new User();
+        user.setUserId(1);
+        int userId = user.getUserId();
+        System.out.println("user: " + user);
+        return userId + "";
+    }
+
+    /**
+     * 传入参数为用户id，然后获取用户信息，最后进入表单页面并进行信息回显
+     * @param userId
+     * @param user
+     * @return
+     */
+    @RequestMapping("/edit/{userId}")
+    public String eidtInfo(@PathVariable("userId") Integer userId, User user){
+        System.out.println("userId: " + userId);
+        user.setUserId(userId);
+        user.setUserName("song");
+        user.setUserPassword("123456");
+        user.setUserEmail("song@163.com");
+        System.out.println(user);
+        return "user/updateUserInfo";
+    }
+
+    /**
+     * 修改用户信息：在前台已经对数据进行验证（用户名、密码、邮箱不为空，两次密码一致，邮箱符合标准），
+     * 只需将传入数据保存返回首页即可
+     * @param userId
+     * @param user
+     * @return
+     */
+    @RequestMapping("/update/{userId}")
+    public String updateInfo(@PathVariable("userId") Integer userId, User user){
+        System.out.println(user);
+        return "index";
+    }
+    @RequestMapping("/login")
+    public String login(User user){
+        return "user/login";
+    }
+
+    /**
+     * 验证用户信息若用户信息通过，则返回首页，否则返回登录页
+     * @param user
+     * @return
+     */
+    @RequestMapping("/userVerify")
+    public String userVerify(User user){
+        System.out.println("user: " + user);
+        return "index";
+    }
+
+    @RequestMapping("/register")
+    public String register(){
+        return "user/register";
+    }
+
+    @RequestMapping("/userRegister")
+    public String userRegister(User user){
+        System.out.println("user: " + user);
+        return "index";
+    }
 }
