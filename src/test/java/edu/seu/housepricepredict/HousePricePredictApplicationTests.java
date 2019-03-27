794c592d756846a5a805005782faa2b7bed4c2bb
Guodong Wu <GuodonWu@163.com>
2019-03-27 19:45:15 +0800

    finish

--- src/test/java/edu/seu/housepricepredict/HousePricePredictApplicationTests.java
+++ src/test/java/edu/seu/housepricepredict/HousePricePredictApplicationTests.java
@@ -27,53 +27,6 @@ public class HousePricePredictApplicationTests {
     @Autowired
     CityService cityService;
 
-
-//    @Test
-//    public void test() throws IOException {
-//        String directoryName = "C:\\Users\\DELL\\Desktop\\项目实训\\数据\\汇总\\全国历史数据";
-//        File file = new File(directoryName);
-//        List<String> list = new ArrayList<>();
-//        File[] provinces = file.listFiles();
-//        for (int i = 0; i < provinces.length; i++) {
-//            File[] data = provinces[i].listFiles();
-//            for (int j = 0; j < data.length; j++) {
-//                if (data[j].getName().matches(".*street.*")) {
-//                    String fileName = directoryName + "\\" +provinces[i].getName() + "\\" + data[j].getName();
-//                    list.add(fileName);
-//                }
-//            }
-//        }
-//
-//        for (String fileName : list) {
-//            importDataService.insertStreetYearPrice(fileName);
-//        }
-//    }
-
-//    @Test
-//    public void insert() throws IOException {
-//        String directoryName = "C:\\Users\\DELL\\Desktop\\项目实训\\数据\\汇总\\全国房价";
-//        File file = new File(directoryName);
-//        List<String> list = new ArrayList<>();
-//        File[] provinces = file.listFiles();
-//        for (int i = 0; i < provinces.length; i++) {
-//            File[] data = provinces[i].listFiles();
-//            for (int j = 0; j < data.length; j++) {
-//                String fileName = directoryName + "\\" +provinces[i].getName() + "\\" + data[j].getName();
-//                list.add(fileName);
-//            }
-//        }
-//
-//        for (String fileName : list) {
-//            importDataService.insertCommunity(fileName);
-//        }
-//    }
-
-//    @Test
-//    public void test01() throws IOException {
-//        String fileName = "C:\\Users\\DELL\\Desktop\\项目实训\\数据\\汇总\\全国历史数据\\江苏\\jiangsu_street.csv";
-//        importDataService.insertStreetYearPrice(fileName);
-//    }
-
     @Test
     public void test() {
 
